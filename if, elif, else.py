ec113bd19e7f4bc7c672c48920ced4e99d963c3b
Viktoriya-max <viktoriyakoloskova20@gmail.com>
2024-10-09 23:43:30 +0300

    Add files via upload

---
+++ if, elif, else.py
@@ -0,0 +1,10 @@
+first=int(input('Введите первое число '))
+second=int(input('Введите второе число '))
+third=int(input('Введите третьу число '))
+if first==second==third:
+    print('Равных чисел 3')
+elif first==second or first==third  or second==third:
+    print('Равных чисел 2')
+else:
+    print('Равных чисел нет 0')
+
